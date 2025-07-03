859d93cc566b46fe254c19ef49337a1087c4c55f
akhatri12-seneca <chhetrissdhish999@gmail.com>
2025-07-03 14:25:23 -0400

    LAB 2 COMPLETE.

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: [Aashish khatri]
+# Author ID: [158418228]
+# Date Created: [2025/07/03]
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
---
+++ lab2g.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Author: [Aashish khatri]
+# Author ID: [158418228]
+# Date Created: [2025/07/03]
+
+import sys
+
+if len(sys.argv) == 1:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
